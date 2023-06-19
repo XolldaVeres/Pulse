9bcbd1ccbaf569015f4a16a2bcd33cfa16f125fd
Xollda <vasya.osipov02@mail.ru>
2023-06-19 18:46:03 +1000

    add secion catalog*script*query

--- src/js/script.js
+++ src/js/script.js
@@ -20,4 +20,24 @@ $(document).ready(function(){
             }
         },]
         });
+        // переключение вкладок с помощью библиотеки jquery
+        $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+            $(this)
+            .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+            .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+        });
+        // скрипт для раскрывающихся ссылок * подробнее и назад
+        // он находится в таком абстрактном калькуляторе и может переиспользоваться в одной строчке без повторений огромных кусков кода
+        function toggleSlide(item) {
+            $(item).each(function(i) {
+                $(this).on('click', function(e) {
+                    e.preventDefault();
+                    $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+                    $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+                })
+            })
+        };
+
+        toggleSlide('.catalog-item__link')
+        toggleSlide('.catalog-item__back');
 });
